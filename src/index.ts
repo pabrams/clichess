4227dc85ca8123a2b8d3bfad3bca4e449a67ab99
Paul Abrams <john.paul.abrams@gmail.com>
2021-11-15 22:33:44 -0500

    refactor: move readStream to separate util file

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 const fetch = require('node-fetch');
 import { Chess } from 'chess.js';
+import { readStream } from './util';
 const headers = { Authorization: 'Bearer ' + process.env.lichessToken };
 
 interface Player {
@@ -21,32 +22,6 @@ let accountPromise = fetch('https://lichess.org/api/account', { headers })
 accountPromise.then(() => {
   console.log("\n^bAnd now, let's see what's on TV^r...\n");
 });
-
-const readStream = (processLine: { (obj: any): void; (arg0: any): void; }) => (response: { 
-  body: { 
-    on: (arg0: string, arg1: { (v: any): void; }) => void;
-  }; 
-}) => {
-  const matcher = /\r?\n/;
-  const decoder = new TextDecoder();
-  let buf: string|undefined = '';
-  return new Promise<void>((resolve, fail) => {
-    response.body.on('data', (v) => {
-      const chunk = decoder.decode(v, { stream: true });
-      buf += chunk;
-      if (buf){
-        const parts = buf.split(matcher);
-        buf = parts.pop();
-        for (const i of parts.filter(p => p)) processLine(JSON.parse(i));
-      }
-    });
-    response.body.on('end', () => {
-      if (buf && buf.length > 0) processLine(JSON.parse(buf));
-      resolve();
-    });
-    response.body.on('error', fail);
-  });
-};
 
 const stream = fetch('https://lichess.org/api/tv/feed');
 
