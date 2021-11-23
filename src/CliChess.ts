0d594bfd042626ab379a909a5da7465ab2917a7d
Paul Abrams <john.paul.abrams@gmail.com>
2021-11-22 21:11:40 -0500

    feat: add command 'analyze'

--- src/CliChess.ts
+++ src/CliChess.ts
@@ -1,22 +1,44 @@
 import { Command } from 'commander';
 import { Feed } from './Feed'
+import fetch from 'cross-fetch';
 
 const headers = { Authorization: 'Bearer ' + process.env.lichessToken };
-const feed_Url = 'https://lichess.org/api/tv/feed';
+const Api_Url = 'https://lichess.org/api';
+const feed_Url = Api_Url + '/tv/feed';
+const cloudEval_Url = Api_Url + '/cloud-eval';
 const prog = new Command();
 prog
   .version('0.0.1')
-  .description('command line client to lichess.org')
-  .option('-n, --name <name>', 'your name', 'pabrams')
-  .option('-d, --debug', 'output debug information')
+  .description('command line client to lichess.org');
+
+prog
+  .command('analyze')
+  .description('analyze the position given as FEN')
+  .argument(
+    '<FEN>', 
+    'the position to analyze, in Forsythe-Edwards Notation'
+  )
+  .action((fen, options, command) => {
+    fetch(cloudEval_Url + '?' + fen)
+      .then((reply) => {
+        console.log('Analysis outcome: ', reply);
+      })
+  });
+
+prog
   .command('feed')
   .description('stream the current tv game from ' + feed_Url)
-  .action((options, command) => {
-    const feed = new Feed(options, command, feed_Url);
-    feed.go();
+  .action((fen, options, command) => {
+      const feed = new Feed(options, command, feed_Url);
+      feed.go();
   });
+
+prog
+  .option('-d, --debug', 'output debug information');
+
 
 prog.parse(process.argv);
 
-
+// sample FEN
+// fen=rnbqkbnr/ppp1pppp/8/3pP3/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 2
 
