e983006717704f2efe9912709ccd8d522be364f5
Paul Abrams <john.paul.abrams@gmail.com>
2021-12-23 03:16:39 -0500

    chore(eslint): fix all auto-fixable

--- src/Tactics.ts
+++ src/Tactics.ts
@@ -1,10 +1,10 @@
 import * as fs from 'fs';
 import { parse } from 'fast-csv';
 import * as path from 'path';
-import * as AsciiBoard from './AsciiBoard';
-import { Chess } from './Chess';
 import * as Blessed from 'blessed';
 import BlessedContrib from 'blessed-contrib';
+import * as AsciiBoard from './AsciiBoard';
+import { Chess } from './Chess';
 
 interface IPuzzle{
   PuzzleId: string,
@@ -13,59 +13,67 @@ interface IPuzzle{
   Rating: string,
   RatingDeviation: string,
   Popularity: string,
-  NbPlays: string, 
+  NbPlays: string,
   Themes: string,
   GameUrl: string
 }
 
 class Puzzle implements IPuzzle {
   PuzzleId = '';
+
   FEN = '';
+
   Moves = '';
+
   Rating = '';
+
   RatingDeviation = '';
+
   Popularity = '';
+
   NbPlays = '';
+
   Themes = '';
+
   GameUrl = '';
 }
-let toMove = "WHITE";
+let toMove = 'WHITE';
 const screen = Blessed.screen({
   smartCSR: true,
-  title: 'CliChess'
+  title: 'CliChess',
 });
-const grid = new BlessedContrib.grid({rows: 12, cols:12, screen: screen});;
+const grid = new BlessedContrib.grid({ rows: 12, cols: 12, screen });
 const boardBox = grid.set(0, 0, 4, 4, Blessed.box, { tags: true });
 const colorToPlayBox = Blessed.text({
   tags: true,
   content: toMove,
 });
-const logBox = grid.set(0,8,4,4, Blessed.box, {
+const logBox = grid.set(0, 8, 4, 4, Blessed.box, {
   label: 'log',
-  tags: true ,
+  tags: true,
   alwaysScroll: true,
   scrollable: true,
   scrollbar: {
     ch: ' ',
-    bg: 'red'
-  }
+    bg: 'red',
+  },
 });
 
 const form = grid.set(0, 4, 4, 4, Blessed.form, {
-  parent: screen, 
+  parent: screen,
   label: 'Move?',
   top: 'center',
-  left: 'center', 
+  left: 'center',
   fg: 'yellow',
   keys: true,
   vi: true,
   border: {
-    type: 'line'
+    type: 'line',
   },
   bg: 'black',
   autoNext: true,
 });
-const yourMove = Blessed.textbox({ 
+const yourMove = Blessed.textbox({
   parent: form,
   name: 'yourMove',
   width: '80%',
@@ -79,14 +87,14 @@ const yourMove = Blessed.textbox({
   tags: true,
   style: {
     fg: 'white',
-    bg: 'black'
+    bg: 'black',
   },
   border: {
-    type: 'line'
+    type: 'line',
   },
   focus: {
-    fg: 'blue'
-  }
+    fg: 'blue',
+  },
 });
 const submit = Blessed.button({
   parent: form,
@@ -95,153 +103,145 @@ const submit = Blessed.button({
   shrink: true,
   bg: 'grey',
   padding: {
-      left: 1,
-      right: 1
+    left: 1,
+    right: 1,
   },
   border: {
-    type: 'line'
+    type: 'line',
   },
   style: {
     border: {
       type: 'line',
-      fg: 'green'
+      fg: 'green',
     },
     hover: {
       bg: 'darkgreen',
-      fg: 'blue'
+      fg: 'blue',
     },
   },
   left: 1,
   bottom: 1,
   height: 3,
   name: 'submit',
-  content: 'submit'
+  content: 'submit',
 });
-const statusBox = grid.set(4,0,2,12, Blessed.box, {
+const statusBox = grid.set(4, 0, 2, 12, Blessed.box, {
   label: 'status',
-  tags: true ,
+  tags: true,
   alwaysScroll: true,
   scrollable: true,
   scrollbar: {
     ch: ' ',
-    bg: 'red'
-  }
+    bg: 'red',
+  },
 });
-let chess:Chess = new Chess();
+const chess:Chess = new Chess();
 const puzzles: IPuzzle[] = [];
 let correctMoves: string[] = [];
 let moveCounter = 0;
-let puzzleIndex = 0;
+const puzzleIndex = 0;
 
 const sideToMove = (puzzle: IPuzzle) => {
-  const blackOrWhite = (puzzle.FEN.split(' ')[1] === 'b') ? 'BLACK': 'WHITE';
+  const blackOrWhite = (puzzle.FEN.split(' ')[1] === 'b') ? 'BLACK' : 'WHITE';
   return blackOrWhite;
-}
+};
 const toggleSideToMove = () => {
-  if (toMove === 'BLACK'){
+  if (toMove === 'BLACK') {
     toMove = 'WHITE';
     colorToPlayBox.setContent('WHITE');
-  }
-  else{
-    toMove = 'BLACK'
+  } else {
+    toMove = 'BLACK';
     colorToPlayBox.setContent('BLACK');
   }
-}
+};
 const blackToMove = () => toMove === 'BLACK';
 
 /**
  * When it's BLACK to move, return prefix "...", otherwise return like "1. "
- * @param sideToMove 
- * @returns 
+ * @param sideToMove
+ * @returns
  */
-const movePrefix = (sideToMove: string) => {
-  return sideToMove === 'BLACK' ? " .." : (moveCounter / 2) + ". ";
-}
+const movePrefix = (sideToMove: string) => (sideToMove === 'BLACK' ? ' ..' : `${moveCounter / 2}. `);
 
 /**
  * PuzzleId,FEN,Moves,Rating,RatingDeviation,Popularity,NbPlays,Themes,GameUrl
  */
 export const readTacticsCsv = () => {
   fs.createReadStream(path.resolve(__dirname, '../lichess_db_puzzle.csv'))
-    .pipe(parse({ headers: false , maxRows: 5 }))
+    .pipe(parse({ headers: false, maxRows: 5 }))
     .on('error', (error: any) => console.error(error))
     .on('data', (row: any) => {
-        const puzzle = new Puzzle();
-        puzzle.PuzzleId = row[0];
-        puzzle.FEN = row[1];
-        puzzle.Moves = row[2];
-        puzzle.Rating = row[3];
-        puzzle.RatingDeviation = row[4];
-        puzzle.Popularity = row[5];e
-        puzzle.NbPlays = row[6];
-        puzzle.Themes = row[7];
-        puzzle.GameUrl = row[8];
-        puzzles.push(puzzle);
+      const puzzle = new Puzzle();
+      puzzle.PuzzleId = row[0];
+      puzzle.FEN = row[1];
+      puzzle.Moves = row[2];
+      puzzle.Rating = row[3];
+      puzzle.RatingDeviation = row[4];
+      puzzle.Popularity = row[5]; e;
+      puzzle.NbPlays = row[6];
+      puzzle.Themes = row[7];
+      puzzle.GameUrl = row[8];
+      puzzles.push(puzzle);
     })
     .on('end', (rowCount: any) => {
-        const puzzle = puzzles[puzzleIndex];
-        chess.load(puzzle.FEN);
-        boardBox.setContent(
-          '\r\n' + AsciiBoard.fromChessJsBoard(chess.board()));
-        correctMoves = puzzle.Moves.split(' ');
-        setTimeout(letComputerMakeMove, 3000);
-        toMove = sideToMove(puzzle);
-        statusLine("Waiting for " + toMove + " to move...");
-        screen.render();
+      const puzzle = puzzles[puzzleIndex];
+      chess.load(puzzle.FEN);
+      boardBox.setContent(
+        `\r\n${AsciiBoard.fromChessJsBoard(chess.board())}`,
+      );
+      correctMoves = puzzle.Moves.split(' ');
+      setTimeout(letComputerMakeMove, 3000);
+      toMove = sideToMove(puzzle);
+      statusLine(`Waiting for ${toMove} to move...`);
+      screen.render();
     });
-}
+};
 const onEveryMove = (move: string, isPlayer: boolean) => {
-  const fullMove = chess.makeMove(move);  
-  if (fullMove){
-    const moveString = movePrefix(toMove) + fullMove.san
+  const fullMove = chess.makeMove(move);
+  if (fullMove) {
+    const moveString = movePrefix(toMove) + fullMove.san;
     logLine(moveString);
-    
-    const movedStringPrefix = isPlayer ? "You played " : "Opponent played ";
+
+    const movedStringPrefix = isPlayer ? 'You played ' : 'Opponent played ';
     statusLine(movedStringPrefix + moveString);
 
     boardBox.setContent(AsciiBoard.fromChessJsBoard(chess.board()));
     screen.focusPush(yourMove);
     toggleSideToMove();
     moveCounter++;
-    if (!isPlayer){
-      statusLine("Your move...");
-    }
-    else{
-      statusLine(`{red-fg}Correct{yellow-fg}!{/yellow-fg}{/red-fg}`);
+    if (!isPlayer) {
+      statusLine('Your move...');
+    } else {
+      statusLine('{red-fg}Correct{yellow-fg}!{/yellow-fg}{/red-fg}');
     }
   }
-}
+};
 const letComputerMakeMove = () => {
   onEveryMove(correctMoves[moveCounter], false);
   screen.render();
-}
-form.on('submit', function (data: any) {
-  if (data.yourMove === correctMoves[moveCounter]){
+};
+form.on('submit', (data: any) => {
+  if (data.yourMove === correctMoves[moveCounter]) {
     onEveryMove(correctMoves[moveCounter], true);
-    if (moveCounter >= correctMoves.length - 1){
-      statusLine(`{blue-fg}YOU WIN!`);
-    }
-    else {
+    if (moveCounter >= correctMoves.length - 1) {
+      statusLine('{blue-fg}YOU WIN!');
+    } else {
       letComputerMakeMove();
     }
     screen.render();
-
-  }
-  else {
-    statusLine("{red-fg}" + data.yourMove + " is incorrect. Try again.")
+  } else {
+    statusLine(`{red-fg}${data.yourMove} is incorrect. Try again.`);
     screen.focusPush(yourMove);
     yourMove.clearValue();
   }
 });
 export const nextPuzzle = () => {
   readTacticsCsv();
-}
+};
 submit.on('press', () => {
   form.submit();
 });
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-  return process.exit(0);
-});
+screen.key(['escape', 'q', 'C-c'], (ch, key) => process.exit(0));
 const logLine = (text: string) => {
   logBox.pushLine(text);
   logBox.setScrollPerc(100);
